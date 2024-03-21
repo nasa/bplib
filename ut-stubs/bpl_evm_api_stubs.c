af121e282a093f260d20174b65de2edede758bc4
krmoore2 <keegan.r.moore@nasa.gov>
2024-03-21 16:24:22 +0000

    Run clang-format on ut-stubs/bpl_evm_api_stubs.c

--- ut-stubs/bpl_evm_api_stubs.c
+++ ut-stubs/bpl_evm_api_stubs.c
@@ -13,7 +13,6 @@
 #include "bpl_evm_api.h"
 #include "utgenstub.h"
 
-
 /*
  * ----------------------------------------------------
  * Generated stub function for BPL_EVM_Deinitialize()
@@ -22,8 +21,7 @@
 void BPL_EVM_Deinitialize(void)
 {
 
-UT_GenStub_Execute(BPL_EVM_Deinitialize, Basic, NULL);
-
+    UT_GenStub_Execute(BPL_EVM_Deinitialize, Basic, NULL);
 }
 
 /*
@@ -31,15 +29,15 @@ UT_GenStub_Execute(BPL_EVM_Deinitialize, Basic, NULL);
  * Generated stub function for BPL_EVM_EventTypeToString()
  * ----------------------------------------------------
  */
-char const * BPL_EVM_EventTypeToString(BPL_EVM_EventType_t Type)
+char const *BPL_EVM_EventTypeToString(BPL_EVM_EventType_t Type)
 {
-UT_GenStub_SetupReturnBuffer(BPL_EVM_EventTypeToString, char const *);
+    UT_GenStub_SetupReturnBuffer(BPL_EVM_EventTypeToString, char const *);
 
-UT_GenStub_AddParam(BPL_EVM_EventTypeToString, BPL_EVM_EventType_t , Type);
+    UT_GenStub_AddParam(BPL_EVM_EventTypeToString, BPL_EVM_EventType_t, Type);
 
-UT_GenStub_Execute(BPL_EVM_EventTypeToString, Basic, NULL);
+    UT_GenStub_Execute(BPL_EVM_EventTypeToString, Basic, NULL);
 
-return UT_GenStub_GetReturnValue(BPL_EVM_EventTypeToString, char const *);
+    return UT_GenStub_GetReturnValue(BPL_EVM_EventTypeToString, char const *);
 }
 
 /*
@@ -49,13 +47,13 @@ return UT_GenStub_GetReturnValue(BPL_EVM_EventTypeToString, char const *);
  */
 BPL_Status_t BPL_EVM_Initialize(BPL_EVM_ProxyCallbacks_t ProxyCallbacks)
 {
-UT_GenStub_SetupReturnBuffer(BPL_EVM_Initialize, BPL_Status_t);
+    UT_GenStub_SetupReturnBuffer(BPL_EVM_Initialize, BPL_Status_t);
 
-UT_GenStub_AddParam(BPL_EVM_Initialize, BPL_EVM_ProxyCallbacks_t , ProxyCallbacks);
+    UT_GenStub_AddParam(BPL_EVM_Initialize, BPL_EVM_ProxyCallbacks_t, ProxyCallbacks);
 
-UT_GenStub_Execute(BPL_EVM_Initialize, Basic, NULL);
+    UT_GenStub_Execute(BPL_EVM_Initialize, Basic, NULL);
 
-return UT_GenStub_GetReturnValue(BPL_EVM_Initialize, BPL_Status_t);
+    return UT_GenStub_GetReturnValue(BPL_EVM_Initialize, BPL_Status_t);
 }
 
 /*
@@ -63,20 +61,19 @@ return UT_GenStub_GetReturnValue(BPL_EVM_Initialize, BPL_Status_t);
  * Generated stub function for BPL_EVM_SendEvent()
  * ----------------------------------------------------
  */
-BPL_Status_t BPL_EVM_SendEvent(uint16_t EventID, BPL_EVM_EventType_t EventType, char const * EventText, ...)
+BPL_Status_t BPL_EVM_SendEvent(uint16_t EventID, BPL_EVM_EventType_t EventType, char const *EventText, ...)
 {
-va_list UtStub_ArgList;
+    va_list UtStub_ArgList;
 
-UT_GenStub_SetupReturnBuffer(BPL_EVM_SendEvent, BPL_Status_t);
+    UT_GenStub_SetupReturnBuffer(BPL_EVM_SendEvent, BPL_Status_t);
 
-UT_GenStub_AddParam(BPL_EVM_SendEvent, uint16_t , EventID);
-UT_GenStub_AddParam(BPL_EVM_SendEvent, BPL_EVM_EventType_t , EventType);
-UT_GenStub_AddParam(BPL_EVM_SendEvent, char const * , EventText);
+    UT_GenStub_AddParam(BPL_EVM_SendEvent, uint16_t, EventID);
+    UT_GenStub_AddParam(BPL_EVM_SendEvent, BPL_EVM_EventType_t, EventType);
+    UT_GenStub_AddParam(BPL_EVM_SendEvent, char const *, EventText);
 
-va_start(UtStub_ArgList, EventText);
-UT_GenStub_Execute(BPL_EVM_SendEvent, Va, NULL, UtStub_ArgList);
-va_end(UtStub_ArgList);
+    va_start(UtStub_ArgList, EventText);
+    UT_GenStub_Execute(BPL_EVM_SendEvent, Va, NULL, UtStub_ArgList);
+    va_end(UtStub_ArgList);
 
-return UT_GenStub_GetReturnValue(BPL_EVM_SendEvent, BPL_Status_t);
+    return UT_GenStub_GetReturnValue(BPL_EVM_SendEvent, BPL_Status_t);
 }
-
