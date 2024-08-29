c6c79ee7a541277d1ad97169b7b60c1bab10fe0a
Cameron Sykes <cameron.j.sykes@nasa.gov>
2024-08-29 15:41:36 +0000

    DTNN-333: Remove structs defined elsewhere

--- ci/em/inc/bplib_em.h
+++ ci/em/inc/bplib_em.h
@@ -39,23 +39,6 @@ typedef enum
     BPLibEM_EventType_CRITICAL = 5
 } BPLib_EM_EventType_t;
 
-// typedef struct
-// {
-//     uint32_t ReturnValue;
-// } BPL_Status_t;
-
-// #define BPL_STATUS_SUCCESS             (0u)
-// #define BPL_STATUS_ERROR_GENERAL       (1u)
-// #define BPL_STATUS_ERROR_INPUT_INVALID (2u)
-// #define BPL_STATUS_ERROR_PROXY_INIT    (3u)
-
-// typedef struct
-// {
-//     BPL_Status_t (*Initialize_Impl)(void);
-//     BPL_Status_t (*SendEvent_Impl)(uint16_t EventID, BPL_EM_EventType_t EventType,
-//                                    char const * EventText, va_list EventTextArgPtr);
-// } BPL_EM_ProxyCallbacks_t;
-
 /* ================== */
 /* Exported Functions */
 /* ================== */
