46240c0d7624e0d338f1f79396eafa284ed6d3f2
Cameron Sykes <cameron.j.sykes@nasa.gov>
2024-09-04 15:36:10 +0000

    DTNN-333: Add EVP unit tests

--- aa/fwp/unit-test/bplib_fwp_test.c
+++ aa/fwp/unit-test/bplib_fwp_test.c
@@ -63,6 +63,16 @@ int64_t BPA_TIMEP_GetHostTime(void)
     return 0;
 }
 
+BPLib_Status_t BPA_EVP_Register(const void* Filters, uint16_t NumEventFilters, uint16_t FilterScheme)
+{
+    return 0;
+}
+
+BPLib_Status_t BPA_EVP_SendEvent(uint16_t EventID, BPLib_EM_EventType_t EventType, char const* EventText)
+{
+    return 0;
+}
+
 /* Test nominal FWP initialization */
 void Test_BPLib_FWP_Init_Nominal(void)
 {
@@ -72,6 +82,8 @@ void Test_BPLib_FWP_Init_Nominal(void)
     Callbacks.BPA_TIMEP_GetHostEpoch = BPA_TIMEP_GetHostEpoch;
     Callbacks.BPA_TIMEP_GetHostTime = BPA_TIMEP_GetHostTime;
     Callbacks.BPA_TIMEP_GetMonotonicTime = BPA_TIMEP_GetMonotonicTime;
+    Callbacks.BPA_EVP_Register  = BPA_EVP_Register;
+    Callbacks.BPA_EVP_SendEvent = BPA_EVP_SendEvent;
 
     UtAssert_INT32_EQ(BPLib_FWP_Init(Callbacks), BPLIB_SUCCESS);
 
@@ -83,6 +95,10 @@ void Test_BPLib_FWP_Init_Nominal(void)
                     "Same BPA_TIMEP_GetHostTime functions");
     UtAssert_True(Callbacks.BPA_TIMEP_GetMonotonicTime == BPLib_FWP_ProxyCallbacks.BPA_TIMEP_GetMonotonicTime, 
                     "Same BPA_TIMEP_GetMonotonicTime functions");
+    UtAssert_True(Callbacks.BPA_EVP_Register == BPLib_FWP_ProxyCallbacks.BPA_EVP_Register,
+                    "Same BPA_EVP_Register function");
+    UtAssert_True(Callbacks.BPA_EVP_SendEvent == BPLib_FWP_ProxyCallbacks.BPA_EVP_SendEvent,
+                    "Same BPA_EVP_SendEvent function");
 }
 
 /* Test FWP initialization with null function */
@@ -121,6 +137,23 @@ void Test_BPLib_FWP_Init_GetMonotonicTimeNull(void)
     UtAssert_INT32_EQ(BPLib_FWP_Init(Callbacks), BPLIB_FWP_CALLBACK_INIT_ERROR);
 }
 
+/* Test FWP initialization with null EVP_Register function */
+void Test_BPLib_FWP_Init_EVP_RegisterNull(void)
+{
+    BPLib_FWP_ProxyCallbacks_t Callbacks;
+
+    Callbacks.BPA_EVP_Register = NULL;
+    UtAssert_INT32_EQ(BPLib_FWP_Init(Callbacks), BPLIB_FWP_CALLBACK_INIT_ERROR);
+}
+
+/* Test FWP initialization with null EVP_SendEvent function */
+void Test_BPLib_FWP_Init_EVP_SendEventNull(void)
+{
+    BPLib_FWP_ProxyCallbacks_t Callbacks;
+
+    Callbacks.BPA_EVP_SendEvent = NULL;
+    UtAssert_INT32_EQ(BPLib_FWP_Init(Callbacks), BPLIB_FWP_CALLBACK_INIT_ERROR);
+}
 
 void TestBplibFwp_Register(void)
 {
@@ -129,4 +162,6 @@ void TestBplibFwp_Register(void)
     UtTest_Add(Test_BPLib_FWP_Init_GetHostEpochNull, BPLib_FWP_Test_Setup, BPLib_FWP_Test_Teardown, "Test_BPLib_FWP_Init_GetHostEpochNull");
     UtTest_Add(Test_BPLib_FWP_Init_GetHostTimeNull, BPLib_FWP_Test_Setup, BPLib_FWP_Test_Teardown, "Test_BPLib_FWP_Init_GetHostTimeNull");
     UtTest_Add(Test_BPLib_FWP_Init_GetMonotonicTimeNull, BPLib_FWP_Test_Setup, BPLib_FWP_Test_Teardown, "Test_BPLib_FWP_Init_GetMonotonicTimeNull");
+    UtTest_Add(Test_BPLib_FWP_Init_EVP_RegisterNull,  BPLib_FWP_Test_Setup, BPLib_FWP_Test_Teardown, "Test_BPLib_FWP_Init_EVP_RegisterNull");
+    UtTest_Add(Test_BPLib_FWP_Init_EVP_SendEventNull, BPLib_FWP_Test_Setup, BPLib_FWP_Test_Teardown, "Test_BPLib_FWP_Init_EVP_SendEventNull");
 }
