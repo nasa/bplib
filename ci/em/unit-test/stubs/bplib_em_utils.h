619400a5b75730a38b44dae7275912909887d33b
Cameron Sykes <cameron.j.sykes@nasa.gov>
2024-09-16 14:26:52 +0000

    Revert "DTNN-333: WIP: Add event checking to BPLib"
    This reverts commit 1aa79de19dd56a487cc7640ed1fba19fd7b4d0d1.

--- ci/em/unit-test/stubs/bplib_em_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef BPLIB_EM_UTILS_H
-#define BPLIB_EM_UTILS_H
-
-/* Macro to get expected event name */
-#define BPLIB_EM_UT_CHECKEVENT_SETUP(Evt, ExpectedEvent, ExpectedFormat) \
-    BPLib_EM_UT_CheckEvent_Setup_Impl(Evt, ExpectedEvent, #ExpectedEvent, ExpectedFormat)
-
-#endif // BPLIB_EM_UTILS_H
