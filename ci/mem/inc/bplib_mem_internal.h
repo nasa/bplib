cf889a00ab44cf77e53c1292794e6565a05c5cca
Grafton Kennedy <grafton.s.kennedy@nasa.gov>
2024-09-17 18:03:27 +0000

    DTNN-328 Label BP_MPOOL_MIN_USER_BLOCK_SIZE as bytes

--- ci/mem/inc/bplib_mem_internal.h
+++ ci/mem/inc/bplib_mem_internal.h
@@ -34,7 +34,7 @@
 /*
  * Minimum size of a generic data block
  */
-#define BP_MPOOL_MIN_USER_BLOCK_SIZE 480
+#define BP_MPOOL_MIN_USER_BLOCK_SIZE 480 // bytes
 
 #define MPOOL_CACHE_CBOR_DATA_SIGNATURE 0x6b243e33
 
