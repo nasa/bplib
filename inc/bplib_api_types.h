b854343e9fb5b323a97ba5a551497c584475afe2
Cameron Sykes <cameron.j.sykes@nasa.gov>
2024-09-19 13:04:32 +0000

    DTNN-333: Make error return codes negative, per convention

--- inc/bplib_api_types.h
+++ inc/bplib_api_types.h
@@ -62,13 +62,13 @@ typedef int32_t BPLib_Status_t;
 #define BPLIB_TIME_UNINIT_ERROR             ((BPLib_Status_t)  -7)
 
 /* Event Management Errors */
-#define BPLIB_EM_ILLEGAL_APP_ID             ((BPLib_Status_t)  6u)
-#define BPLIB_EM_UNKNOWN_FILTER             ((BPLib_Status_t)  7u)
-#define BPLIB_EM_BAD_ARGUMENT               ((BPLib_Status_t)  8u)
-#define BPLIB_EM_INVALID_PARAMETER          ((BPLib_Status_t)  9u)
-#define BPLIB_EM_APP_NOT_REGISTERED         ((BPLib_Status_t) 10u)
-#define BPLIB_EM_APP_SQUELCHED              ((BPLib_Status_t) 11u)
-#define BPLIB_EM_STRING_TRUNCATED           ((BPLib_Status_t) 12u)
+#define BPLIB_EM_ILLEGAL_APP_ID             ((BPLib_Status_t) -8u)
+#define BPLIB_EM_UNKNOWN_FILTER             ((BPLib_Status_t) -9u)
+#define BPLIB_EM_BAD_ARGUMENT               ((BPLib_Status_t) -10u)
+#define BPLIB_EM_INVALID_PARAMETER          ((BPLib_Status_t) -11u)
+#define BPLIB_EM_APP_NOT_REGISTERED         ((BPLib_Status_t) -12u)
+#define BPLIB_EM_APP_SQUELCHED              ((BPLib_Status_t) -13u)
+#define BPLIB_EM_STRING_TRUNCATED           ((BPLib_Status_t) -14u)
 
 #ifdef __cplusplus
 } // extern "C"
