7533219c7fdcec1fc283df307190864f582a7e48
Cameron Sykes <cameron.j.sykes@nasa.gov>
2024-09-20 19:37:14 +0000

    DTNN-344: Create payloads for DP commands

---
+++ aa/nc/inc/bplib_nc_payloads.h
@@ -0,0 +1,246 @@
+/*
+ * NASA Docket No. GSC-18,587-1 and identified as “The Bundle Protocol Core Flight
+ * System Application (BP) v6.5”
+ *
+ * Copyright © 2020 United States Government as represented by the Administrator of
+ * the National Aeronautics and Space Administration. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef BPLIB_NC_PAYLOADS_H
+#define BPLIB_NC_PAYLOADS_H
+
+#include "bplib_fwp.h"
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_AddAllApplicationsCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_StartAllApplicationsCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_VerifyBundleStorageCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_InitBundleStorageCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_VerifyBundleMetadataCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_RebuildBundleMetadataCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_ClearVolatileCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_ReloadSavedDataCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_ResetAllCountersCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_ResetCounterCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_ResetSourceCountersCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_ResetBundleCountersCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_ResetErrorCountersCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_AddApplicationCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_RemoveApplicationCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_SetRegistrationStateCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_StartApplicationCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_StopApplicationCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_AddAuthSourcesCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_RemoveAuthSourcesCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_AddAuthCustodySourcesCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_RemoveAuthCustodySourcesCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_AddAuthCustodiansCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_RemoveAuthCustodiansCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_AddAuthReportToEidCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_RemoveAuthReportToEidCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_AddLatencyCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_RemoveLatencyCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_ContactSetupCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_ContactStartCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_ContactStopCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_ContactTeardownCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_AddMibArrayKeyCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_RemoveMibArrayKeyCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_SetMibItemCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_AddStorageAllocationCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_RemoveStorageAllocationCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_PerformSelfTestCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_SendNodeMibConfigHkCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_SendSourceMibConfigHkCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_SendNodeMibCountersHkCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_SendSourceMibCountersHkCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_SendStorageHkCmd_Payload_t;
+
+typedef struct
+{
+    uint8_t ExampleParameter;
+} BPLib_SendChannelContactStatHkCmd_Payload_t;
+
+#endif // BPLIB_NC_PAYLOADS_H
