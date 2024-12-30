583817d093f8d4b16213393b20cdf177dec1d91f
Joseph Martin <joseph.v.martin@nasa.gov>
2024-12-30 14:34:12 +0000

    Correctly name job functions

--- bplib-ci-ideas/qm/src/qm_jobs.c
+++ bplib-ci-ideas/qm/src/qm_jobs.c
@@ -9,35 +9,35 @@ static bool jobs_table_init_done = false;
 /* 
 ** Sandwiches - These are the entry points to jobs being run in QM.
 */
-BPLib_QM_JobState_t BPLib_Job_CLA_Ingress(BPLib_QM_QueueTable_t* tbl, BPLib_Bundle_t* bundle)
+BPLib_QM_JobState_t BPLib_Job_BI_Ingress(BPLib_QM_QueueTable_t* tbl, BPLib_Bundle_t* bundle)
 {
     printf("CLA TO BI\n");
     sleep(1);
     return STATE_BI_TO_EBP;
 }
 
-BPLib_QM_JobState_t BPLib_Job_BI_Ingress(BPLib_QM_QueueTable_t* tbl, BPLib_Bundle_t* bundle)
+BPLib_QM_JobState_t BPLib_Job_EBP_Ingress(BPLib_QM_QueueTable_t* tbl, BPLib_Bundle_t* bundle)
 {
     printf("BI TO EBP\n");
     sleep(1);
     return STATE_EBP_TO_CT;
 }
 
-BPLib_QM_JobState_t BPLib_Job_EBP_Ingress(BPLib_QM_QueueTable_t* tbl, BPLib_Bundle_t* bundle)
+BPLib_QM_JobState_t BPLib_Job_CT_Ingress(BPLib_QM_QueueTable_t* tbl, BPLib_Bundle_t* bundle)
 {
     printf("EBP TO CT\n");
     sleep(1);
     return STATE_CT_TO_CACHE;
 }
 
-BPLib_QM_JobState_t BPLib_Job_CT_Ingress(BPLib_QM_QueueTable_t* tbl, BPLib_Bundle_t* bundle)
+BPLib_QM_JobState_t BPLib_Job_CACHE_Ingress(BPLib_QM_QueueTable_t* tbl, BPLib_Bundle_t* bundle)
 {
     printf("CT TO CACHE\n");
     sleep(1);
     return STATE_CACHE_TO_CT;
 }
 
-BPLib_QM_JobState_t BPLib_Job_CACHE(BPLib_QM_QueueTable_t* tbl, BPLib_Bundle_t* bundle)
+BPLib_QM_JobState_t BPLib_Job_CACHE_Egress(BPLib_QM_QueueTable_t* tbl, BPLib_Bundle_t* bundle)
 {
     printf("CACHE TO CT\n");
     sleep(1);
@@ -74,11 +74,11 @@ void BPLib_QM_JobTableInit()
 
     /* job_funcs is an array of sandwhich-pointers */
     memset(job_funcs, 0, sizeof(job_funcs));
-    job_funcs[STATE_CLA_TO_BI] = BPLib_Job_CLA_Ingress;
-    job_funcs[STATE_BI_TO_EBP] = BPLib_Job_BI_Ingress;
-    job_funcs[STATE_EBP_TO_CT] = BPLib_Job_EBP_Ingress;
-    job_funcs[STATE_CT_TO_CACHE] = BPLib_Job_CT_Ingress;
-    job_funcs[STATE_CACHE_TO_CT] = BPLib_Job_CACHE;
+    job_funcs[STATE_CLA_TO_BI] = BPLib_Job_BI_Ingress;
+    job_funcs[STATE_BI_TO_EBP] = BPLib_Job_EBP_Ingress;
+    job_funcs[STATE_EBP_TO_CT] = BPLib_Job_CT_Ingress;
+    job_funcs[STATE_CT_TO_CACHE] = BPLib_Job_CACHE_Ingress;
+    job_funcs[STATE_CACHE_TO_CT] = BPLib_Job_CACHE_Egress;
     job_funcs[STATE_CT_TO_EBP] = BPLib_Job_CT_Egress;
     job_funcs[STATE_EBP_TO_BI] = BPLib_Job_EBP_Egress;
     job_funcs[STATE_BI_TO_CLA] = BPLib_Job_BI_Egress;
