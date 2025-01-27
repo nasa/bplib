1cd709ce1f2c4b2bc5d77a9a4e42f0416215313c
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-01-27 16:13:30 +0000

    DTNN-993: Allow wildcards in actual or reference EID in EID matching function

--- ci/eid/src/bplib_eid.c
+++ ci/eid/src/bplib_eid.c
@@ -32,8 +32,9 @@ bool BPLib_EID_IsValid(BPLib_EID_t EID)
 {
     bool IsValid;
 
-    if (EID.Scheme == 1 || EID.Scheme == 2)
+    if (EID.Scheme == BPLIB_EID_SCHEME_INT || EID.Scheme == BPLIB_EID_SCHEME_STR)
     {
+        /* NOTE: 2-digit IPNs will have a default Authority of 0 */
         if (EID.Authority == 0)
         {
             if (EID.Node == 0)
@@ -66,12 +67,12 @@ bool BPLib_EID_IsMatch(BPLib_EID_t EID_Actual, BPLib_EID_t EID_Reference)
 
     if (EID_Actual.Scheme == EID_Reference.Scheme)
     { /* The EID schemes are compatible for comparison */
-        if (EID_Actual.Authority == EID_Reference.Authority || EID_Reference.Authority == BPLIB_EID_WILDCARD)
-        { /* Authority in EIDs match or the reference authority is a wildcard */
-            if (EID_Actual.Node == EID_Reference.Node || EID_Reference.Node == BPLIB_EID_WILDCARD)
-            { /* Node in EIDs match or the reference node is a wildcard */
-                if (EID_Actual.Service == EID_Reference.Service || EID_Reference.Service == BPLIB_EID_WILDCARD)
-                { /* Services in EIDs match or the reference service is a wildcard */
+        if (EID_Actual.Authority == EID_Reference.Authority || EID_Actual.Authority == BPLIB_EID_WILDCARD || EID_Reference.Authority == BPLIB_EID_WILDCARD)
+        { /* Authority in EIDs match or the either EID's authority is a wildcard */
+            if (EID_Actual.Node == EID_Reference.Node || EID_Actual.Node == BPLIB_EID_WILDCARD || EID_Reference.Node == BPLIB_EID_WILDCARD)
+            { /* Node in EIDs match or either EID's node is a wildcard */
+                if (EID_Actual.Service == EID_Reference.Service || EID_Actual.Service == BPLIB_EID_WILDCARD || EID_Reference.Service == BPLIB_EID_WILDCARD)
+                { /* Services in EIDs match or either EID's service is a wildcard */
                     IsMatch = true;
                 }
             }
