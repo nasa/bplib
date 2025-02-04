4d0cca756a5fb3ef658a05f62959fa01a4ce39d7
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-02-04 21:29:25 +0000

    DTNN-1128: Leave EID validity testing to AS_SetCounter

--- aa/as/src/bplib_as.c
+++ aa/as/src/bplib_as.c
@@ -45,50 +45,39 @@ void BPLib_AS_Increment(BPLib_EID_t EID, BPLib_AS_Counter_t Counter, uint32_t Am
 {
     BPLib_Status_t Status;
 
-    if (BPLib_EID_IsValid(EID))
-    {
-        if (BPLib_EID_IsMatch(EID, BPLIB_EID_INSTANCE))
-        { /* Increment a node counter */
-            /* Prevent modification of counters from other tasks while modifying them */
-            BPLib_AS_LockCounters();
+    if (BPLib_EID_IsMatch(EID, BPLIB_EID_INSTANCE))
+    { /* Increment a node counter */
+        /* Prevent modification of counters from other tasks while modifying them */
+        BPLib_AS_LockCounters();
 
-            /* Use BPLib_AS_SetCounter to evaluate source MIB array index and counter ranges */
-            Status = BPLib_AS_SetCounter(EID, Counter, BPLib_AS_NodeCountersPayload.NodeCounters[Counter] + Amount);
-            if (Status != BPLIB_SUCCESS)
-            {
-                BPLib_EM_SendEvent(BPLIB_AS_SET_CTR_ERR_EID,
-                                    BPLib_EM_EventType_ERROR,
-                                    "Could not set node counter %d to %d, RC = %d",
-                                    Counter,
-                                    BPLib_AS_NodeCountersPayload.NodeCounters[Counter] + Amount,
-                                    Status);
-            }
-
-            /* Allow counters to be modified by other tasks after operation has finished */
-            BPLib_AS_UnlockCounters();
+        /* Use BPLib_AS_SetCounter to evaluate source MIB array index and counter ranges */
+        Status = BPLib_AS_SetCounter(EID, Counter, BPLib_AS_NodeCountersPayload.NodeCounters[Counter] + Amount);
+        if (Status != BPLIB_SUCCESS)
+        {
+            BPLib_EM_SendEvent(BPLIB_AS_SET_CTR_ERR_EID,
+                                BPLib_EM_EventType_ERROR,
+                                "Could not increment node counter %d to %d, RC = %d",
+                                Counter,
+                                BPLib_AS_NodeCountersPayload.NodeCounters[Counter] + Amount,
+                                Status);
         }
-        else
-        { /* Increment a source counter */
-            /* Prevent modification of counters from other tasks while modifying them */
-            BPLib_AS_LockCounters();
 
-            /* Allow counters to be modified by other tasks after operation has finished */
-            BPLib_AS_UnlockCounters();
-        }
+        /* Allow counters to be modified by other tasks after operation has finished */
+        BPLib_AS_UnlockCounters();
     }
     else
-    {
-        BPLib_EM_SendEvent(BPLIB_AS_INVALID_EID_ERR_EID,
-                            BPLib_EM_EventType_ERROR,
-                            "Could not set counter %d due to an invalid EID",
-                            Counter);
+    { /* Increment a source counter */
+        /* Prevent modification of counters from other tasks while modifying them */
+        BPLib_AS_LockCounters();
+
+        /* Allow counters to be modified by other tasks after operation has finished */
+        BPLib_AS_UnlockCounters();
     }
 }
 
 void BPLib_AS_Decrement(BPLib_EID_t EID, BPLib_AS_Counter_t Counter, uint32_t Amount)
 {
     BPLib_Status_t Status;
-    uint8_t MibArrayIndex;
 
     if (BPLib_EID_IsMatch(EID, BPLIB_EID_INSTANCE))
     { /* Increment a node counter */
@@ -101,7 +90,7 @@ void BPLib_AS_Decrement(BPLib_EID_t EID, BPLib_AS_Counter_t Counter, uint32_t Am
         {
             BPLib_EM_SendEvent(BPLIB_AS_SET_CTR_ERR_EID,
                                 BPLib_EM_EventType_ERROR,
-                                "Could not set counter %d to %d, RC = %d",
+                                "Could not decrement node counter %d to %d, RC = %d",
                                 Counter,
                                 BPLib_AS_NodeCountersPayload.NodeCounters[Counter] - Amount,
                                 Status);
