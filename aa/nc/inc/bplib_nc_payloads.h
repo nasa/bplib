612c5ab0a62842875f603c5a07c4e1a94f8ecd54
Grafton Kennedy <grafton.s.kennedy@nasa.gov>
2025-02-05 13:48:16 +0000

    DTNN-1132 Update Per-Source MIB Config Tbl for EID struct

--- aa/nc/inc/bplib_nc_payloads.h
+++ aa/nc/inc/bplib_nc_payloads.h
@@ -27,6 +27,7 @@
 
 #include "bplib_api_types.h"
 #include "bplib_cfg.h"
+#include "bplib_eid.h"
 
 /* =================== */
 /* Payload Definitions */
@@ -313,8 +314,14 @@ typedef struct
 */
 typedef struct
 {
-    char        SrcEID[BPLIB_MAX_EID_LENGTH];
-    char        EIDPattern[BPLIB_MAX_EID_LENGTH];
+    /**
+     * The SrcEID field is the EID pattern.
+     * The EIDPattern field was a string value of "ipn" or "dtn". It is now
+     * the BPLib_EID_Scheme_t enum in BPLib_EID_PatternMatch_t.
+     */
+    // char        SrcEID[BPLIB_MAX_EID_LENGTH];
+    // char        EIDPattern[BPLIB_MAX_EID_LENGTH];
+    BPLib_EID_PatternMatch_t SrcEIDs[BPLIB_MAX_NUM_MIB_PS];
     uint32_t    ParamSetMaxLifetime;
     uint32_t    ParamSetMaxBSRGenerationRate;
     uint32_t    ParamSetMaxCBRGenerationRate;
