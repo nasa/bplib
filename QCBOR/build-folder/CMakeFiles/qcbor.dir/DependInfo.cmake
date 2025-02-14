a1f5cc526a3891b5d33ba7f18939aba3ffaef178
Grafton Kennedy <grafton.s.kennedy@nasa.gov>
2025-02-14 13:53:47 +0000

    DTNN-639 Add pre-built QCBOR lib as a workaround

---
+++ QCBOR/build-folder/CMakeFiles/qcbor.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/gskenned/repos/nasa-gitlab/gsfc-dtn/dtn-cfs/libs/bplib/QCBOR/src/UsefulBuf.c" "/home/gskenned/repos/nasa-gitlab/gsfc-dtn/dtn-cfs/libs/bplib/QCBOR/build-folder/CMakeFiles/qcbor.dir/src/UsefulBuf.c.o"
+  "/home/gskenned/repos/nasa-gitlab/gsfc-dtn/dtn-cfs/libs/bplib/QCBOR/src/ieee754.c" "/home/gskenned/repos/nasa-gitlab/gsfc-dtn/dtn-cfs/libs/bplib/QCBOR/build-folder/CMakeFiles/qcbor.dir/src/ieee754.c.o"
+  "/home/gskenned/repos/nasa-gitlab/gsfc-dtn/dtn-cfs/libs/bplib/QCBOR/src/qcbor_decode.c" "/home/gskenned/repos/nasa-gitlab/gsfc-dtn/dtn-cfs/libs/bplib/QCBOR/build-folder/CMakeFiles/qcbor.dir/src/qcbor_decode.c.o"
+  "/home/gskenned/repos/nasa-gitlab/gsfc-dtn/dtn-cfs/libs/bplib/QCBOR/src/qcbor_encode.c" "/home/gskenned/repos/nasa-gitlab/gsfc-dtn/dtn-cfs/libs/bplib/QCBOR/build-folder/CMakeFiles/qcbor.dir/src/qcbor_encode.c.o"
+  "/home/gskenned/repos/nasa-gitlab/gsfc-dtn/dtn-cfs/libs/bplib/QCBOR/src/qcbor_err_to_str.c" "/home/gskenned/repos/nasa-gitlab/gsfc-dtn/dtn-cfs/libs/bplib/QCBOR/build-folder/CMakeFiles/qcbor.dir/src/qcbor_err_to_str.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../inc"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ QCBOR/build-folder/CMakeFiles/qcbor.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qcbor.dir/src/UsefulBuf.c.o"
+  "CMakeFiles/qcbor.dir/src/ieee754.c.o"
+  "CMakeFiles/qcbor.dir/src/qcbor_decode.c.o"
+  "CMakeFiles/qcbor.dir/src/qcbor_encode.c.o"
+  "CMakeFiles/qcbor.dir/src/qcbor_err_to_str.c.o"
+  "libqcbor.a"
+  "libqcbor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/qcbor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
