ebf80c3867ea3b84411c622f3e0a4f152dcc3485
Sara Garcia-Beech <sara.garcia-beech@nasa.gov>
2025-02-28 17:33:24 +0000

    DTNN-1210 Added channel table for PI tests

--- bpa/pi/unit-test/utilities/bplib_pi_test_utils.c
+++ bpa/pi/unit-test/utilities/bplib_pi_test_utils.c
@@ -23,13 +23,15 @@
 */
 
 #include "bplib_pi_test_utils.h"
-
+#include "bplib_fwp.h"
 
 /*
 ** Global Data
 */
 
 BPLib_Instance_t BplibInst;
+BPLib_PI_ChannelTable_t TestChanTbl;
+
 
 /*
 ** Function Definitions
@@ -38,6 +40,8 @@ BPLib_Instance_t BplibInst;
 void BPLib_PI_Test_Setup(void)
 {
     memset(&BplibInst, 0, sizeof(BPLib_Instance_t));
+
+    BPLib_FWP_ConfigPtrs.ChanTblPtr = &TestChanTbl;
 
     /* Initialize test environment to default state for every test */
     UT_ResetState(0);
