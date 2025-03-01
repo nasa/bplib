09e58f20e5e7876d5cc3ed3503c99256eb543d86
krmoore2 <keegan.r.moore@nasa.gov>
2025-03-01 16:41:13 +0000

    DTNN-1178 Add null input tests for BPLib_CLA_Egress

--- cla/unit-test/bplib_cla_test.c
+++ cla/unit-test/bplib_cla_test.c
@@ -43,15 +43,6 @@
 //     UtAssert_INT32_EQ(BPLib_CLA_Ingress(ContId, Bundle, Size, Timeout), BPLIB_SUCCESS);
 // }
 
-// void Test_BPLib_CLA_Egress_Nominal(void)
-// {
-//     uint32_t ContId = 0;
-//     uint8_t Bundle[]= {0x9F, 0x0, 0x0, 0x0, 0x0};
-//     size_t Size;
-//     uint32_t Timeout = 0;
-//     UtAssert_INT32_EQ(BPLib_CLA_Egress(ContId, Bundle, &Size, Timeout), BPLIB_SUCCESS);
-// }
-
 // void Test_BPLib_CLA_Ingress_MsgData(void)
 // {
 //     uint32_t ContId = 0;
@@ -81,15 +72,6 @@
 //     UtAssert_INT32_EQ(BPLib_CLA_Ingress(ContId, Bundle, Size, Timeout), BPLIB_SUCCESS);
 // }
 
-// void Test_BPLib_CLA_Egress_NULLBundle(void)
-// {
-//     uint32_t ContId = 0;
-//     uint8_t* Bundle = NULL;
-//     size_t Size;
-//     uint32_t Timeout = 0;
-//     UtAssert_INT32_EQ(BPLib_CLA_Egress(ContId, Bundle, &Size, Timeout), BPLIB_SUCCESS);
-// }
-
 // void Test_BPLib_CLA_ContactsTblValidateFunc_Nominal(void)
 // {
 //     BPLib_CLA_ContactsTable_t TestTblData;
@@ -110,15 +92,100 @@
 //                                                 BPLIB_TABLE_OUT_OF_RANGE_ERR_CODE);
 // }
 
+
+void Test_BPLib_CLA_Egress_NullInstanceInputError(void)
+{
+    BPLib_Status_t ReturnStatus;
+    uint32_t ContId = 0;
+    uint8_t OutputBundleBuffer[30];
+    size_t NumBytesCopiedToOutputBuf;
+    size_t OutputBundleBufferLength = sizeof(OutputBundleBuffer);
+    uint32_t Timeout = 0;
+
+    ReturnStatus = BPLib_CLA_Egress(NULL,
+                                    ContId,
+                                    OutputBundleBuffer,
+                                    &NumBytesCopiedToOutputBuf,
+                                    OutputBundleBufferLength,
+                                    Timeout);
+
+    UtAssert_INT32_EQ(ReturnStatus, BPLIB_NULL_PTR_ERROR);
+}
+
+
+void Test_BPLib_CLA_Egress_NullBundleOutBufferError(void)
+{
+    BPLib_Status_t ReturnStatus;
+    BPLib_Instance_t Instance;
+    uint32_t ContId = 0;
+    uint8_t OutputBundleBuffer[30];
+    size_t NumBytesCopiedToOutputBuf;
+    size_t OutputBundleBufferLength = sizeof(OutputBundleBuffer);
+    uint32_t Timeout = 0;
+
+    ReturnStatus = BPLib_CLA_Egress(&Instance,
+                                    ContId,
+                                    NULL,
+                                    &NumBytesCopiedToOutputBuf,
+                                    OutputBundleBufferLength,
+                                    Timeout);
+
+    UtAssert_INT32_EQ(ReturnStatus, BPLIB_NULL_PTR_ERROR);
+}
+
+
+void Test_BPLib_CLA_Egress_NullSizeBufferError(void)
+{
+    BPLib_Status_t ReturnStatus;
+    BPLib_Instance_t Instance;
+    uint32_t ContId = 0;
+    uint8_t OutputBundleBuffer[30];
+    // size_t NumBytesCopiedToOutputBuf;
+    size_t OutputBundleBufferLength = sizeof(OutputBundleBuffer);
+    uint32_t Timeout = 0;
+
+    ReturnStatus = BPLib_CLA_Egress(&Instance,
+                                    ContId,
+                                    OutputBundleBuffer,
+                                    NULL,
+                                    OutputBundleBufferLength,
+                                    Timeout);
+
+    UtAssert_INT32_EQ(ReturnStatus, BPLIB_NULL_PTR_ERROR);
+}
+
+
+void Test_BPLib_CLA_Egress_Nominal(void)
+{
+    BPLib_Status_t ReturnStatus;
+    BPLib_Instance_t Instance;
+    uint32_t ContId = 0;
+    uint8_t OutputBundleBuffer[30];
+    size_t NumBytesCopiedToOutputBuf;
+    size_t OutputBundleBufferLength = sizeof(OutputBundleBuffer);
+    uint32_t Timeout = 0;
+
+    ReturnStatus = BPLib_CLA_Egress(&Instance,
+                                    ContId,
+                                    OutputBundleBuffer,
+                                    &NumBytesCopiedToOutputBuf,
+                                    OutputBundleBufferLength,
+                                    Timeout);
+
+    UtAssert_INT32_EQ(ReturnStatus, BPLIB_SUCCESS);
+}
+
 void TestBplibCla_Register(void)
 {
     // UtTest_Add(Test_BPLib_CLA_Init, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_Init");
     // UtTest_Add(Test_BPLib_CLA_Ingress_Nominal, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_Ingress_Nominal");
-    // UtTest_Add(Test_BPLib_CLA_Egress_Nominal, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_Egress_Nominal");
     // UtTest_Add(Test_BPLib_CLA_Ingress_MsgData, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_Ingress_MsgData");
     // UtTest_Add(Test_BPLib_CLA_Ingress_NonControlMsg, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_Ingress_NonControlMsg");
     // UtTest_Add(Test_BPLib_CLA_Ingress_NULLBundle, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_Ingress_NULLBundle");
-    // UtTest_Add(Test_BPLib_CLA_Egress_NULLBundle, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_Egress_NULLBundle");    
     // UtTest_Add(Test_BPLib_CLA_ContactsTblValidateFunc_Nominal, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_ContactsTblValidateFunc_Nominal");
     // UtTest_Add(Test_BPLib_CLA_ContactsTblValidateFunc_Invalid, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_ContactsTblValidateFunc_Invalid");
+    UtTest_Add(Test_BPLib_CLA_Egress_NullInstanceInputError, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_Egress_NullInstanceInputError");
+    UtTest_Add(Test_BPLib_CLA_Egress_NullBundleOutBufferError, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_Egress_NullBundleOutBufferError");
+    UtTest_Add(Test_BPLib_CLA_Egress_NullSizeBufferError, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_Egress_NullSizeBufferError");
+    UtTest_Add(Test_BPLib_CLA_Egress_Nominal, BPLib_CLA_Test_Setup, BPLib_CLA_Test_Teardown, "Test_BPLib_CLA_Egress_Nominal");
 }
