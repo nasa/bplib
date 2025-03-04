9bc45f0bdf90849e9d43fb4ceadcc9ed7b483370
krmoore2 <keegan.r.moore@nasa.gov>
2025-03-04 23:41:13 +0000

    DTNN-1178 Remove unused BPLib_CBOR_CopyOutEncodedData

--- ci/cbor/src/bplib_cbor_internal.h
+++ ci/cbor/src/bplib_cbor_internal.h
@@ -231,21 +231,6 @@ BPLib_Status_t BPLib_CBOR_EncodePrimary(QCBOREncodeContext* Context,
     size_t* NumBytesCopied);
 
 /**
- * \brief Copies the pre-encoded data out of a bundle, block by block.
- *
- * \param[in] Context (QCBOREncodeContext*) QCBOR encode context instance pointer
- * \param[in] Bundle (BPLib_Bundle_t*) Pointer to the bundle from which to copy the data.
- * \param[in] Offset (uint64_t) Offset into the user data
- * \param[in] NumBytesToCopy (uint64_t) Number of bytes to copy
- *
- * \return Status of the operation.
- */
-BPLib_Status_t BPLib_CBOR_CopyOutEncodedData(QCBOREncodeContext* Context,
-    BPLib_Bundle_t* Bundle,
-    uint64_t Offset,
-    uint64_t NumBytesToCopy);
-
-/**
  * \brief Copies the Primary Block out of a stored bundle,
  *        or encodes the primary (if never previously encoded)
  *
