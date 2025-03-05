4b6aa473e37f5ebc958d20561ea2c5390393e4ec
krmoore2 <keegan.r.moore@nasa.gov>
2025-03-05 23:53:23 +0000

    DTNN-1178 First stab at payload encode logic
    I'd like a chance to clean this code up before the team shames me here lol

--- ci/cbor/src/bplib_cbor_encode_payload.c
+++ ci/cbor/src/bplib_cbor_encode_payload.c
@@ -7,14 +7,15 @@ BPLib_Status_t BPLib_CBOR_EncodePayload(BPLib_Bundle_t* StoredBundle,
 {
     BPLib_Status_t ReturnStatus;
     BPLib_Status_t PayloadDataCopyStatus;
-    // size_t StartOffset;
-    // size_t EndOffset;
-    size_t PayloadDataBytesCopied;
     UsefulBuf OutputSubBuffer;
     QCBOREncodeContext Context;
     UsefulBuf InitStorage;
     UsefulBufC FinishBuffer;
     QCBORError QcborStatus;
+    uintptr_t CurrentOutputBufferAddr;
+    size_t TotalBytesCopied;
+    size_t BytesLeftInOutputBuffer;
+    uint8_t DataSizeByte;
 
     if (StoredBundle == NULL)
     {
@@ -32,22 +33,29 @@ BPLib_Status_t BPLib_CBOR_EncodePayload(BPLib_Bundle_t* StoredBundle,
     {
 
         /*
-        ** Initialize the encoder.
-        ** TODO: move this after the openArray, and create the array ourselves
+        ** Jam in an "open definite array" character
+        ** Major Type: 4 (array)
+        ** Additional Info: number of data items in array (6 total)
+        **  1. Block Type
+        **  2. Block Num
+        **  3. Block Processing Flags
+        **  4. CRC Type
+        **  5. Block-Specific Data (ADU)
+        **  6. CRC Value
+        ** 0b100_00110 == 0x86
         */
-        InitStorage.ptr = OutputBuffer;
-        InitStorage.len = OutputBufferSize;
+        CurrentOutputBufferAddr = (uintptr_t)(OutputBuffer);
+        *(uint8_t*)CurrentOutputBufferAddr = 0x86;
+        TotalBytesCopied = 1;
+        CurrentOutputBufferAddr++;
+        BytesLeftInOutputBuffer = OutputBufferSize - TotalBytesCopied;
+
+        /*
+        ** Initialize the encoder (encoding just the initial items before the ADU)
+        */
+        InitStorage.ptr = CurrentOutputBufferAddr;
+        InitStorage.len = BytesLeftInOutputBuffer;
         QCBOREncode_Init(&Context, InitStorage);
-
-        /*
-        ** Grab the start offset, to be used to calculate the total encoded size
-        */
-        // StartOffset = QCBOREncode_Tell(&Context);
-
-        /*
-        ** Open Canonical/Payload Block Array
-        */
-        QCBOREncode_OpenArray(&Context);
 
         /*
         ** Add our block header data
@@ -58,43 +66,200 @@ BPLib_Status_t BPLib_CBOR_EncodePayload(BPLib_Bundle_t* StoredBundle,
         QCBOREncode_AddUInt64(&Context, StoredBundle->blocks.PayloadHeader.CrcType);
 
         /*
-        ** Add the ADU data
+        ** Finish encoding, and check for errors
         */
-        QCBOREncode_OpenBytes(&Context, &OutputSubBuffer);
-
-        PayloadDataCopyStatus = BPLib_MEM_CopyOutFromOffset(StoredBundle,
-            StoredBundle->blocks.PayloadHeader.DataOffsetStart,
-            StoredBundle->blocks.PayloadHeader.DataOffsetSize,
-            OutputSubBuffer.ptr,
-            OutputSubBuffer.len);
-
-        if (PayloadDataCopyStatus == BPLIB_SUCCESS)
+        FinishBuffer.len = 0;
+        FinishBuffer.ptr = NULL;
+        QcborStatus = QCBOREncode_Finish(&Context, &FinishBuffer);
+        if (QcborStatus != QCBOR_SUCCESS)
         {
-            PayloadDataBytesCopied = StoredBundle->blocks.PayloadHeader.DataOffsetSize;
+            *NumBytesCopied = 0;
+            return BPLIB_ERROR;
         }
         else
         {
-            PayloadDataBytesCopied = 0;
+            TotalBytesCopied += FinishBuffer.len;
         }
 
-        QCBOREncode_CloseBytes(&Context, PayloadDataBytesCopied);
+        /*
+        ** Jam in an "open byte string" character
+        ** Major Type: 2 (byte string)
+        ** Additional Info: number of bytes in the string
+        **  - If number of bytes in the string is less than 24:
+        **      - the next 5 bits describe the length
+        **  - If number of bytes in the string is 25 to 0xFF
+        **      - the next 5 bits should be set to 24
+        **      - the next 1 byte should describe the length
+        **  - If number of bytes in the string is 0x100 to 0xFFFF
+        **      - the next 5 bits should be set to 25
+        **      - the next 2 bytes should describe the length
+        **  - If number of bytes in the string is 0x1.0000 to 0xFFFF.FFFF
+        **      - the next 5 bits should be set to 26
+        **      - the next 4 bytes should describe the length
+        **  - If number of bytes in the string is 0x1.0000.0000 to 0xFFFF.FFFF.FFFF.FFFF
+        **      - the next 5 bits should be set to 27
+        **      - the next 8 bytes should describe the length
+        */
+        if (StoredBundle->blocks.PayloadHeader.DataOffsetSize < 24)
+        {
+            *(uint8_t*)CurrentOutputBufferAddr = (0b010 << 5) | StoredBundle->blocks.PayloadHeader.DataOffsetSize;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+        }
+        else if (StoredBundle->blocks.PayloadHeader.DataOffsetSize < 0x100)
+        {
+            *(uint8_t*)CurrentOutputBufferAddr = (0b010 << 5) | 24;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0xFF);
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+        }
+        else if (StoredBundle->blocks.PayloadHeader.DataOffsetSize < 0x10000)
+        {
+            *(uint8_t*)CurrentOutputBufferAddr = (0b010 << 5) | 25;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0xFF00) >> 8;
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0x00FF);
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+        }
+        else if (StoredBundle->blocks.PayloadHeader.DataOffsetSize < 0x100000000)
+        {
+            *(uint8_t*)CurrentOutputBufferAddr = (0b010 << 5) | 26;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0xFF000000) >> 24;
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0x00FF0000) >> 16;
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0x0000FF00) >> 8;
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0x000000FF);
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+        }
+        else
+        {
+            *(uint8_t*)CurrentOutputBufferAddr = (0b010 << 5) | 27;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0xFF00000000000000) >> 56;
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0x00FF000000000000) >> 48;
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0x0000FF0000000000) >> 40;
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0x000000FF00000000) >> 32;
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0x00000000FF000000) >> 24;
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0x0000000000FF0000) >> 16;
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0x000000000000FF00) >> 8;
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+
+            DataSizeByte = (uint8_t) (StoredBundle->blocks.PayloadHeader.DataOffsetSize & 0x00000000000000FF);
+            *(uint8_t*)CurrentOutputBufferAddr = DataSizeByte;
+            TotalBytesCopied++;
+            CurrentOutputBufferAddr++;
+            BytesLeftInOutputBuffer--;
+        }
+
+        /*
+        ** Add the ADU data
+        */
+        PayloadDataCopyStatus = BPLib_MEM_CopyOutFromOffset(StoredBundle,
+            StoredBundle->blocks.PayloadHeader.DataOffsetStart,
+            StoredBundle->blocks.PayloadHeader.DataOffsetSize,
+            CurrentOutputBufferAddr,
+            BytesLeftInOutputBuffer);
+
+        if (PayloadDataCopyStatus == BPLIB_SUCCESS)
+        {
+            CurrentOutputBufferAddr += StoredBundle->blocks.PayloadHeader.DataOffsetSize;
+            BytesLeftInOutputBuffer -= StoredBundle->blocks.PayloadHeader.DataOffsetSize;
+            TotalBytesCopied += StoredBundle->blocks.PayloadHeader.DataOffsetSize;
+        }
+        else
+        {
+            *NumBytesCopied = 0;
+            return PayloadDataCopyStatus;
+        }
+
+
+        /*
+        ** Initialize the encoder (to encode the CRC)
+        */
+        InitStorage.ptr = CurrentOutputBufferAddr;
+        InitStorage.len = BytesLeftInOutputBuffer;
+        QCBOREncode_Init(&Context, InitStorage);
 
         /*
         ** Add the CRC
         */
         /* TODO: calculate the CRC first */
         (void) BPLib_CBOR_EncodeCrcValue(&Context, 0, StoredBundle->blocks.PayloadHeader.CrcType);
-
-        /*
-        ** Close Array
-        */
-        QCBOREncode_CloseArray(&Context);
-
-        /*
-        ** Calculate the total encoded size
-        */
-        // EndOffset = QCBOREncode_Tell(&Context);
-
 
         /*
         ** Finish encoding, and check for errors
@@ -105,16 +270,17 @@ BPLib_Status_t BPLib_CBOR_EncodePayload(BPLib_Bundle_t* StoredBundle,
         if (QcborStatus != QCBOR_SUCCESS)
         {
             *NumBytesCopied = 0;
-            ReturnStatus = BPLIB_SUCCESS;
+            return BPLIB_ERROR;
         }
         else
         {
-            *NumBytesCopied = FinishBuffer.len;
-            ReturnStatus = BPLIB_SUCCESS;
+            TotalBytesCopied += FinishBuffer.len;
+            CurrentOutputBufferAddr += FinishBuffer.len;
+            BytesLeftInOutputBuffer -= FinishBuffer.len;
         }
 
-        // *NumBytesCopied = EndOffset - StartOffset;
-        // ReturnStatus = BPLIB_SUCCESS;
+        *NumBytesCopied = TotalBytesCopied;
+        ReturnStatus = BPLIB_SUCCESS;
     }
 
     return ReturnStatus;
