46a57ddff7c7d9c334f16c10ee996ad0e681d615
Joseph Martin <joseph.v.martin@nasa.gov>
2025-05-19 15:13:23 +0000

    DTNN-1321 Bump version to 18

--- version/inc/bplib_version.h
+++ version/inc/bplib_version.h
@@ -36,7 +36,7 @@
 /*
  * Development Build Macro Definitions
  */
-#define BPLIB_BUILD_NUMBER (17u) /*!< Development Build: Number of development builds (not commits) since baseline */
+#define BPLIB_BUILD_NUMBER (18u) /*!< Development Build: Number of development builds (not commits) since baseline */
 
 /**
  * @brief Max Version String length.
