417a55e239238d2fa82b96e2023dd9689d3cc577
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-07-09 21:02:31 +0000

    DTNN-1382: Only 1 contact exists without an SB CLA

--- inc/bplib_cfg.h
+++ inc/bplib_cfg.h
@@ -57,7 +57,7 @@ extern "C" {
  *          This drives the number of entries in the CLA configuration
  *          tables, as well as the number of CLA In/Out tasks in BPNode
  */
-#define BPLIB_MAX_NUM_CONTACTS 2
+#define BPLIB_MAX_NUM_CONTACTS                  1
 
 /**
  * \brief Maximum number of destination EID patterns per contact
